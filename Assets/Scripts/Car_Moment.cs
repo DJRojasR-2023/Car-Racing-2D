4e1b312f4eebdc4dd29d2d6d4bf2e739cf17220f
Darwin <darwin.rojas@unmsm.edu.pe>
2023-10-27 23:02:03 -0500

    Update Car_Moment.cs

--- Assets/Scripts/Car_Moment.cs
+++ Assets/Scripts/Car_Moment.cs
@@ -6,7 +6,7 @@ public class Car_Moment : MonoBehaviour
 {
     public Transform cartransform;
     public float speed = 0.5f;
-    // Start is called before the first frame update
+    // Start is called before the first frame update 
     void Start()
     {
         cartransform = GetComponent<Transform>();
